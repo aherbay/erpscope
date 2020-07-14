20a778fd912af41b940e72b0ba842c0199790610
Alexandre Herbay <alexandre.herbay@gmail.com>
2020-07-14 01:29:28 -0400

    trying fix pipe

--- R/plot_difference_by_region.R
+++ R/plot_difference_by_region.R
@@ -49,7 +49,7 @@ plot_difference_by_region  <- function( data,
       plotname = paste(Sys.Date(),"_",deparse(substitute(data)),"_",number_of_subjects,"PPTS_ERP_DIFF_",rlang::quo_text(conditionToPlot_enq),"_",rlang::quo_text(levelA_enq),"-", rlang::quo_text(levelB_enq) ,sep="")
   }
 
-  data_reduced <- data %>% dplyr::select( !! group_var_enq, Time, Electrode , !! vary_enq ,!! conditionToPlot_enq,!! ant_levels_enq,!! med_levels_enq)
+  data_reduced <- data dplyr::%>% dplyr::select( !! group_var_enq, Time, Electrode , !! vary_enq ,!! conditionToPlot_enq,!! ant_levels_enq,!! med_levels_enq)
   print("Data selected")
   data_reduced <- filter(data_reduced, !! conditionToPlot_enq %in% c(rlang::quo_text(levelA_enq), rlang::quo_text(levelB_enq)) ) %>% droplevels()
   print("Data reduced")
@@ -215,7 +215,7 @@ plot_difference_by_region  <- function( data,
   }
 
   message("assembling topoplot")
-  topoplot <- ggarrange(plotlist=topo_ggplots, nrow = 1, ncol = length(time_windows))
+  topoplot <- ggpubr::ggarrange(plotlist=topo_ggplots, nrow = 1, ncol = length(time_windows))
 
   message(paste(Sys.time(),"assembling all plots"))
 
