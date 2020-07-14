99b03939bc2c7405c46a276b009f0e60ed973942
Alexandre Herbay <alexandre.herbay@gmail.com>
2020-07-14 16:45:28 -0400

    TYPO CORRECTION 2

--- R/baseline_correction.R
+++ R/baseline_correction.R
@@ -12,7 +12,7 @@ baseline_correction  <- function( data,
       curr_condition_mean  <- mean(data$Voltage[ data$Electrode == electrode_list[curr_elec]  & data$Time > baseline[1] & data$Time < baseline[2]  & data[,conditionToAdjust] == condition_values[curr_condition] ])
       diff_curr_condition <- curr_condition_mean - overall_elec_mean_voltage
 
-      data$NewVoltage[electrode_list[curr_elec]  & data[,conditionToAdjust] == condition_values[curr_condition]  ] <- data$Voltage[ electrode_list[curr_elec]  & data[,conditionToAdjust] == condition_values[curr_condition] ] - diff_curr_condition
+      data$NewVoltage[  data$Electrode == electrode_list[curr_elec]  & data[,conditionToAdjust] == condition_values[curr_condition]  ] <- data$Voltage[ data$Electrode == electrode_list[curr_elec]  & data[,conditionToAdjust] == condition_values[curr_condition] ] - diff_curr_condition
 
     }
 
