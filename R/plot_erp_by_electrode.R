38fd848a235d6fe4fa7dcc8d6a1a7d2b6884e8f2
Alexandre Herbay <alexandre.herbay@gmail.com>
2020-07-14 17:04:46 -0400

    trying to get y

--- R/plot_erp_by_electrode.R
+++ R/plot_erp_by_electrode.R
@@ -156,7 +156,7 @@ plot_erp_by_electrode<- function( data,
 
 
       message("Saving plot to file")
-      print(tempo$layout$panel_scales_y[[1]]$range$range)
+      print(ggplot_build(tempo)$layout$panel_scales_y[[1]]$range$range)
 
 
       ggsave(tempo, filename=paste(plotname,'.',output_type, sep=''), width = 22, height = 18)
