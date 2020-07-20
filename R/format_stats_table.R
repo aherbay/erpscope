5a6d901e8fba9fb044a96dd60a01842dfa07b2b1
Alexandre Herbay <alexandre.herbay@gmail.com>
2020-07-20 15:24:19 -0400

    first version of generate_ERP_stats_table

---
+++ R/format_stats_table.R
@@ -0,0 +1,61 @@
+format_stats_table <- function( capture, model_description , outputname) {
+
+    # parse html file of multiple lines
+    newHtml<- ""
+    for(i in 1:length(capture)) {
+
+      newHtml<- paste (newHtml,capture[i] )
+    }
+
+    # transform it back in html
+    my_df <- as.data.frame(read_html(newHtml) %>% rvest::html_table(fill=TRUE))
+
+    # remove top line with useless info and bottom lines too
+    my_df <- my_df[-c(1:6,8:9),]
+
+    #gather pvalues threshold and remove bottom two lines
+    pvaluesthresholds <- my_df[c(nrow(my_df)),2]
+    my_df <- my_df[-c(nrow(my_df)),]
+    my_df <- my_df[-c(nrow(my_df)),]
+
+    library(kableExtra)
+
+
+    styling_function_1 <- function(x) {
+      kableExtra::cell_spec(x, "html",
+                            bold = ifelse( grepl("**",x, fixed = T) , TRUE, FALSE),
+                            color = ifelse( grepl("*",x, fixed = T), "black", "#bfbfbf") ,
+                            background = ifelse( grepl("*",x, fixed = T) & grepl("-",x, fixed = T) & grepl("(",x, fixed = T), "#d6ecfb", ifelse(grepl("*",x, fixed = T) & grepl("(",x, fixed = T), "#fbe5d6", "#ffffff")) )
+    }
+
+    styling_function_2 <- function(x) {
+      kableExtra::cell_spec(x, "html",
+                            bold = ifelse( grepl("**",x, fixed = T) , TRUE, FALSE),
+                            color = ifelse( grepl("*",x, fixed = T) & grepl("-",x, fixed = T) & grepl("(",x, fixed = T), "#2372A7",
+                                            ifelse(grepl("*",x, fixed = T) & grepl("(",x, fixed = T), "#B40000", "#cccccc")) )
+    }
+
+
+    table_title <- paste("<font color='black'> <b> Table for model:", model_description,"</b></font>")
+
+    my.names <- my_df[1,]
+    my.names$X1 <- "Coefficients"
+    colnames(my_df) <- my.names
+    my_df <- my_df[-c(1),]
+
+    my_df %>% mutate_at(c(2:ncol(my_df))  , styling_function_2  ) %>% kable(format = "html", escape = F, caption = table_title) %>%
+      kable_styling("striped", full_width = F) %>% column_spec(1, bold = T, color="black")  %>%
+      footnote(general = paste("stars:", pvaluesthresholds)
+      ) %>%  save_kable(file = outputname)
+
+}
+
+
+
+
+# drafts
+
+
+#aa <- newHtml %>% htmlTable()
+
+
--- R/generate_ERP_stats_table.R
+++ R/generate_ERP_stats_table.R
@@ -1,83 +1,101 @@
-
-
-# function generate_ERP_stats_table
-
-# input : dataset , time_step, min_time, max_time, DV, model_structure, model_type, output_name
-
-# output:  html file with the models
-
-
-
-# Example: 
-
-# generate_ERP_stats_table( data = pop2_B1, time_step=50, min_time=-250, max_time=700,
-#                          model_structure = "Voltage ~ MM_RAW * anteriority_3l.z *  mediality_a.z  + (1+MM_RAW|Subject)",
-#                          model_type="lm",
-#                          output_name="MM_RAW.lm.-250ms_700B1.html")
+#' Generate color coded stats table for ERPs
+#'
+#' Description to come
+#'
+#' @param data dataframe containing eeg data
+#' @param model_structure string with the lmer model
+#' @param output_name string for the output name
+#' @return An HTML file containing thes stats tables
+#' @export
 
 
 
-
-model_central_elec_anteriority <- function (data, time_step, min_time, max_time, model_structure, model_type, output_name) {
-
-    data <- subset(data, Electrode %in% c('A'))
-    generate_ERP_stats_table( data = pop2_B1, time_step=50, min_time=-250, max_time=700,
-                              model_structure = model_structure,
-                              model_type=model_type,
-                            output_name=output_name)
-
-}
-
-
-model_lat_elec_hemi <- function (data, time_step, min_time, max_time, model_structure, model_type, output_name) {
+generate_ERP_stats_table <- function (data,
+                                      model_structure,
+                                      output_name,
+                                      timeWindowMode,
+                                      model_type = "lm",
+                                      custom_TW =  list(c(-300,-150),c(-150,50),c(50,200),c(200,300),c(300,500),c(500,700),c(700,900)),
+                                      time_step,
+                                      min_time,
+                                      max_time
+) {
 
 
-
-}
-
-
-
-
-generate_ERP_stats_table <- function (data, time_step, min_time, max_time, model_structure, model_type, output_name) {
-  
-  if((max_time - min_time)%%time_step != 0){
-    stop('max_time should be a multiple of the time_step')
+  if(timeWindowMode== 'byStep') {
+    if((max_time - min_time)%%time_step != 0){
+      stop('max_time should be a multiple of the time_step')
+    }
   }
-  
+
   #create models lists
   models <- list()
   anovas <- list()
   anova_tables <- list()
   model_index <- 1
   models_names <- c()
-  
+  captures <- c()
+
   # for each time window
-  for(lowBound in seq(min_time, max_time-time_step, by=time_step)) {
-    #
-    print(paste("Processing ",lowBound, "ms - ", Sys.time()))
-    models[[model_index]] = lmer(as.formula(model_structure),  data= subset(data, Time > lowBound & Time < (lowBound+time_step) ))
-    models_names[model_index] <- paste(as.character(lowBound),"-",as.character(lowBound+time_step),"ms",sep="")
-    
-    if(model_type=="lm") {
-      #summary(models[[model_index]])
-    } else {
-      if (model_type=="anova") {
-        anovas[[model_index]] <- anova(models[[model_index]])
+  if(timeWindowMode== 'byStep') {
+
+    for(lowBound in seq(min_time, max_time-time_step, by=time_step)) {
+      #
+      message(paste(Sys.time(),"- Processing",lowBound, "ms"))
+      models[[model_index]] = lmerTest::lmer(as.formula(model_structure),  data= subset(data, Time > lowBound & Time < (lowBound+time_step) ))
+      models_names[model_index] <- paste(as.character(lowBound)," to ",as.character(lowBound+time_step),"ms",sep="")
+
+      if(model_type=="lm") {
+        #summary(models[[model_index]])
+      } else {
+        if (model_type=="anova") {
+          anovas[[model_index]] <- car::Anova(models[[model_index]])
+        }
       }
+      model_index <- model_index +1
+
     }
-    model_index <- model_index +1
-    
+
+  } else {
+
+    for(tw in custom_TW) {
+      #
+      message(paste(Sys.time(),"- Processing",tw[1], "ms to", tw[2],"ms"))
+      captures[model_index] <- capture.output( models[[model_index]] <-  lmerTest::lmer(as.formula(model_structure),  data= subset(data, Time > tw[1] & Time < tw[2] )))
+      models_names[model_index] <- paste(as.character(tw[1])," to ",as.character(tw[2]),"ms",sep="")
+      print(captures[model_index])
+
+      if(model_type=="lm") {
+        #summary(models[[model_index]])
+        #pdf(paste("Check",models_names[model_index],'.pdf', sep=''))
+        #check_figure <- capture.output( performance::check_model(models[[model_index]]) ) # a essayer
+        #ggplot2::ggsave(plot= check_figure ,filename=paste("Check",models_names[model_index],'.pdf', sep=''), width = 22, height = 18)
+        #dev.off()
+        #saveRDS(check_figure,"check_figure.RDS")
+      } else {
+        if (model_type=="anova") {
+          anovas[[model_index]] <- car::Anova(models[[model_index]])
+        }
+      }
+      model_index <- model_index +1
+
+    }
+
+    #print(captures)
+
+
   }
-  
-  # generate table 
+
+
+  # generate table
   if(model_type=="lm") {
     #print(models_names)
-    saveRDS(models, paste(output_name, "current_lm_models.RDS"))
-    
+    #saveRDS(models, paste(output_name, "current_lm_models.RDS"))
+
     for(i in 1:length(models)){
       class(models[[i]]) <- "lmerMod"
     }
-    
+
     default_arg <- list( type="html",
                          out= output_name,
                          intercept.bottom = F,
@@ -88,26 +106,33 @@ generate_ERP_stats_table <- function (data, time_step, min_time, max_time, model
                          single.row = T,
                          star.cutoffs = c(0.05, 0.01, 0.001)
     )
+
     arguments_list  <-  c(models, default_arg)
     #do.call("stargazer",arguments_list)
-    stargazer(models,type="html",
-              out= output_name,
-              intercept.bottom = F,
-              intercept.top = T,
-              column.labels= models_names,
-              digits=2,
-              model.names = T,
-              single.row = T,
-              star.cutoffs = c(0.05, 0.01, 0.001))
+    message(paste(Sys.time(),"- Creating stats summary table"))
+
+    capture <- capture.output(stargazer::stargazer(models,type="html",
+                                                   out= output_name,
+                                                   intercept.bottom = F,
+                                                   intercept.top = T,
+                                                   column.labels= models_names,
+                                                   digits=2,
+                                                   model.names = T,
+                                                   single.row = T,
+                                                   star.cutoffs = c(0.05, 0.01, 0.001)))
+    message(paste(Sys.time(),"- Formating stats summary table"))
+    format_stats_table(capture,model_structure,output_name)
+
+
   }
-  
-  
+
+
   if(model_type=="anova") {
-    
+
     #print(models_names)
     for(i in 1:length(models)){
       print(anovas[[i]])
-      
+
       #cat(paste("\n",models_names[i],"\n"), file = paste(output_name,".html",sep = ""), append=TRUE)
       anova_tables[[i]] <- kable(anovas[[i]], digits = 4, format = "html", caption = models_names[i])
       ##%>%
@@ -116,9 +141,5 @@ generate_ERP_stats_table <- function (data, time_step, min_time, max_time, model
     saveRDS(anova_tables, "00_ResourceScripts/anova_tables.RDS")
     render("00_ResourceScripts/prepare_anova_output.Rmd", output_file = paste(output_name,".html",sep=""),params = list(anovas=anova_tables))
   }
-  
+
 }
-
-
-
-
